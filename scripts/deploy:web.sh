2f752d8258105b450a6ca4b3d368ca1bc9ab139d
Team LevX <team@levx.app>
2020-10-15 07:01:36 +0900

    Update deploy script

--- scripts/deploy:web.sh
+++ scripts/deploy:web.sh
@@ -6,9 +6,12 @@ if [ -z $BRANCH ] || [ $BRANCH != "dev" ]; then
     echo "switch branch to dev"
     exit 1
 fi
+git push origin dev
 
 gh-pages -d web-build --remote=gh-pages
+
 git checkout master
 git merge dev --no-edit
 git push origin master
+
 git checkout dev

21480bbd1592dae7e110fb00f3cefc8f30b600d4
Team LevX <team@levx.app>
2020-10-15 03:34:32 +0900

    Add deploy script

---
+++ scripts/deploy:web.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+BRANCH=$(git branch --show-current)
+
+if [ -z $BRANCH ] || [ $BRANCH != "dev" ]; then
+    echo "switch branch to dev"
+    exit 1
+fi
+
+gh-pages -d web-build --remote=gh-pages
+git checkout master
+git merge dev

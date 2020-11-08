159b395c434ad8af1ca15139615f21334cf61bb6
Team LevX <team@levx.app>
2020-11-08 22:33:55 +0900

    Support EIP-1193

--- src/components/web/ConnectToWallet.tsx
+++ src/components/web/ConnectToWallet.tsx
@@ -1,37 +1,39 @@
-import React, { useCallback, useContext } from "react";
+import React, { useContext } from "react";
 import { Image, View } from "react-native";
 
 import * as Linking from "expo-linking";
 
 import { IS_DESKTOP, Spacing } from "../../constants/dimension";
 import { GlobalContext } from "../../context/GlobalContext";
+import useColors from "../../hooks/useColors";
 import Button from "../Button";
 
 const ConnectWallet = () => {
+    const { primary } = useColors();
     const { darkMode } = useContext(GlobalContext);
-    const onPress = useCallback(async () => {
+    const onPress = async () => {
         if (window.ethereum) {
-            await window.ethereum.enable();
+            await window.ethereum.request({ method: "eth_requestAccounts" });
         } else {
             await Linking.openURL("https://metamask.io/");
         }
-    }, [window.ethereum]);
-    const title = window.ethereum ? "Connect" : "Install MetaMask";
+    };
+    const metaMask = window.ethereum.isMetaMask;
+    const source = metaMask
+        ? darkMode
+            ? require("../../../assets/metamask-dark.png")
+            : require("../../../assets/metamask.png")
+        : require("../../../assets/sushiswap.jpg");
     return (
         <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-            <Image
-                source={
-                    darkMode ? require("../../../assets/metamask-dark.png") : require("../../../assets/metamask.png")
-                }
-                style={{ width: 223, height: 183, marginBottom: Spacing.huge }}
-            />
+            <Image source={source} style={{ width: metaMask ? 223 : 200, height: metaMask ? 183 : 200 }} />
             <Button
                 size={"large"}
-                color={"#e2761b"}
+                color={metaMask ? "#e2761b" : primary}
                 onPress={onPress}
-                title={title}
+                title={window.ethereum ? "Connect" : "Install MetaMask"}
                 containerStyle={{ width: IS_DESKTOP ? 440 : "100%" }}
-                style={{ marginHorizontal: Spacing.normal }}
+                style={{ marginTop: Spacing.large, marginHorizontal: Spacing.normal }}
             />
         </View>
     );

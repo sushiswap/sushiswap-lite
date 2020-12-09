38d3e30a5921b135999edce11f28dec616b369ff
Team LevX <team@levx.app>
2020-12-09 09:37:00 +0900

    Add WBTC to the base tokens

--- src/hooks/useAllCommonPairs.tsx
+++ src/hooks/useAllCommonPairs.tsx
@@ -3,6 +3,7 @@ import { useCallback } from "react";
 import { ChainId, Currency, ETHER, Fetcher, Pair, Token, WETH } from "@sushiswap/sdk";
 import { ethers } from "ethers";
 
+const WBTC = new Token(ChainId.MAINNET, "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599", 8, "WBTC", "Wrapped BTC");
 const DAI = new Token(ChainId.MAINNET, "0x6B175474E89094C44Da98b954EedeAC495271d0F", 18, "DAI", "Dai Stablecoin");
 const USDC = new Token(ChainId.MAINNET, "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48", 6, "USDC", "USD//C");
 const USDT = new Token(ChainId.MAINNET, "0xdAC17F958D2ee523a2206206994597C13D831ec7", 6, "USDT", "Tether USD");
@@ -10,9 +11,9 @@ const SUSHI = new Token(ChainId.MAINNET, "0x6B3595068778DD592e39A122f4f5a5cF09C9
 const YAM = new Token(ChainId.MAINNET, "0x0e2298E3B3390e3b945a5456fBf59eCc3f55DA16", 18, "YAM", "YAM");
 const AMPL = new Token(ChainId.MAINNET, "0xD46bA6D942050d489DBd938a2C909A5d5039A161", 9, "AMPL", "Ampleforth");
 
-const BASES_TO_CHECK_TRADES_AGAINST = [WETH[ChainId.MAINNET], DAI, USDC, USDT, SUSHI, YAM];
+const BASES_TO_CHECK_TRADES_AGAINST = [WETH[ChainId.MAINNET], WBTC, DAI, USDC, USDT, SUSHI, YAM];
 const CUSTOM_BASES = {
-    [AMPL.address]: [DAI, WETH[ChainId.MAINNET]]
+    [AMPL.address]: [DAI, WETH[ChainId.MAINNET], WBTC]
 };
 
 function wrappedCurrency(currency: Currency | undefined): Token | undefined {

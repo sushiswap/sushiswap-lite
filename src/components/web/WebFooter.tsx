d0f8d6719c3daea1b10eeb911b7c2393b64fb831
Team LevX <team@levx.app>
2020-12-17 20:09:31 +0900

    Add locale fr

--- src/components/web/WebFooter.tsx
+++ src/components/web/WebFooter.tsx
@@ -6,7 +6,6 @@ import Constants from "expo-constants";
 
 import { Spacing } from "../../constants/dimension";
 import useLinker from "../../hooks/useLinker";
-import Column from "../Column";
 import FlexView from "../FlexView";
 import SocialIcons from "../SocialIcons";
 import Text from "../Text";
@@ -14,35 +13,39 @@ import Text from "../Text";
 const flags = {
     us: require("../../../assets/flags/us.png"),
     cn: require("../../../assets/flags/cn.png"),
-    kr: require("../../../assets/flags/kr.png")
+    kr: require("../../../assets/flags/kr.png"),
+    fr: require("../../../assets/flags/fr.png")
 };
 
-const WebFooter = () => {
+const WebFooter = ({ simple = false }) => {
     const onPressAlchemy = useLinker(
         "https://dashboard.alchemyapi.io/signup?referral=429fb682-0d85-40ab-ad88-daf847cf7c63",
         "",
         "_blank"
     );
     return (
-        <Column noTopMargin={true}>
-            <View style={{ width: "100%", padding: Spacing.normal, alignItems: "center" }}>
-                <SocialIcons />
-                <TouchableHighlight onPress={onPressAlchemy}>
-                    <Image
-                        source={require("../../../assets/alchemy.png")}
-                        style={{ width: 188, height: 40, marginTop: Spacing.tiny }}
-                    />
-                </TouchableHighlight>
-                <Text note={true} style={{ marginTop: Spacing.tiny }}>
-                    Built with ❤️ by SushiSwap (v{Constants.manifest.version})
-                </Text>
-                <FlexView style={{ marginTop: Spacing.small }}>
-                    <Flag name={"us"} locale={"en"} />
-                    <Flag name={"cn"} locale={"zh"} />
-                    <Flag name={"kr"} locale={"ko"} />
-                </FlexView>
-            </View>
-        </Column>
+        <View style={{ width: "100%", padding: Spacing.normal, alignItems: "center" }}>
+            {!simple && (
+                <>
+                    <SocialIcons />
+                    <TouchableHighlight onPress={onPressAlchemy}>
+                        <Image
+                            source={require("../../../assets/alchemy.png")}
+                            style={{ width: 188, height: 40, marginTop: Spacing.tiny }}
+                        />
+                    </TouchableHighlight>
+                </>
+            )}
+            <Text note={true} style={{ marginTop: Spacing.tiny }}>
+                Built with ❤️ by SushiSwap (v{Constants.manifest.version})
+            </Text>
+            <FlexView style={{ marginTop: Spacing.small }}>
+                <Flag name={"us"} locale={"en"} />
+                <Flag name={"fr"} locale={"fr"} />
+                <Flag name={"cn"} locale={"zh"} />
+                <Flag name={"kr"} locale={"ko"} />
+            </FlexView>
+        </View>
     );
 };
 
